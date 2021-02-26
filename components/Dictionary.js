6f9cdb0035b0ec6ab8f7774d1b1438570894e56f
monmima <monmima@protonmail.com>
2021-02-26 17:56:25 -0500

    Dictionary.js, index.html: Dictionary.js component now can handle many different instances of itself with correct handling of links

--- components/Dictionary.js
+++ components/Dictionary.js
@@ -1,5 +1,5 @@
 Vue.component("dictionary", {
-    props: ["name", "link"],
+    props: ["id", "name", "link"],
     data() {
         return {
             fullQuery: this.link
@@ -12,19 +12,17 @@ Vue.component("dictionary", {
 
     methods: {
         handleDictionaryLink() {
-            const test = document.querySelector("input");
+            const myInput = document.querySelector(`input.${this.id}`);
 
-            document.querySelector("#linka").setAttribute("href", this.link + test.value);
+            document.querySelector(`#${this.id}`).setAttribute("href", this.link + myInput.value);
             return;
         }
     },
     // the only difference with a component instead of having the code directly in your HTML is the fact you need to create a template
     template: `
         <div class="m-3">
-            <input v-on:keydown="handleDictionaryLink" />
+            <input :class="this.id" v-on:keyup="handleDictionaryLink" />
 
-            <p><a id="linka" :href="fullQuery" class="btn btn-warning" :title="this.name">Search {{ this.name }}</a></p>
+            <p><a :id="this.id" :href="fullQuery" class="btn btn-warning" :title="this.name">Search {{ this.name }}</a></p>
         </div>`
-    
-
 });

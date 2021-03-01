2414113d387d35fa35157440375439960858a871
monmima <monmima@protonmail.com>
2021-02-28 19:38:58 -0500

    Dictionary2.js component now fully functional

--- components/Dictionary2.js
+++ components/Dictionary2.js
@@ -4,34 +4,40 @@ Vue.component("dictionary-2", {
         return {
             fullQuery: this.link,
             dictionaries: [
-                { name: "test1", link: "https://www.duckduckgo.com/zzzz" },
-                { name: "test2", link: "https://www.startpage.com/zzzz" },
-                { name: "test3", link: "https://www.duckduckgo.com/zzzz" }
+                { name: "Wikipedia FR", baselink: "fr.wikipedia.org/wiki/zzzz" },
+                { name: "Wikipedia IT", baselink: "it.wikipedia.org/wiki/zzzz" },
+                { name: "Wikipedia EN", baselink: "en.wikipedia.org/wiki/zzzz" }
             ]
         }
     },
 
-    created() {
+    mounted() {
         console.log("Dictionary created!");
+
+        // to clean the href attribute from the start
+        // how this works:
+        // the method below is shared between the list of methods and the lifecycle hook thanks to the href keyword; it just wouldn't work without the this keyword
+        this.handleDictionaryLink();
     },
 
     methods: {
         handleDictionaryLink() {
+            // storing the input box as a variable
             const myInput = document.querySelector(`#${this.id} input`);
 
-            //  groupe de boutons
-            const allButtons2 = document.querySelectorAll(`#${this.id} div a`);
+            // storing the buttons as an array
+            const allButtons = document.querySelectorAll(`#${this.id} div a`);
 
-            for (item of allButtons2) {
+            // replacing all of the placeholders with the input box value
+            for (item of allButtons) {
                 let currentLink = item.getAttribute("baselink");
-                currentLink = currentLink.replace("zzzz", "");
+                currentLink = currentLink.replace("zzzz", myInput.value);
 
-                item.setAttribute("href", currentLink + myInput.value);
+                item.setAttribute("href", currentLink);
             }
-
-            return;
         }
     },
+
     // the only difference with a component instead of having the code directly in your HTML is the fact you need to create a template
     template:
         `<div class="m-3" :id="this.id">
@@ -39,7 +45,7 @@ Vue.component("dictionary-2", {
             <input v-on:keyup="handleDictionaryLink" />
 
             <div v-for="dictionary in dictionaries">
-                <a :baselink="dictionary.link" :href="dictionary.link" class="btn btn-warning" :title="dictionary.name">{{ dictionary.name }}</a>
+                <a :baselink="dictionary.baselink" :href="dictionary.baselink" class="btn btn-warning" :title="dictionary.name">{{ dictionary.name }}</a>
             </div>
 
         </div>`

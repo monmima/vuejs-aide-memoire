4e930463320c61832fe3c64dcc1180ccd669daf8
monmima <monmima@protonmail.com>
2021-03-05 20:49:52 -0500

    finished Metadata.js component

--- components/Metadata.js
+++ components/Metadata.js
@@ -10,30 +10,40 @@ Vue.component("metadata", {
         console.log("Dice created!");
     },
     mounted() {
-        this.updateLengthDesc();
+        this.updateLengthDesc(50, 60, '#metatitle');
+        this.updateLengthDesc(120, 158, '#metadescription');
     },
     methods: {
-        updateLengthDesc() {
-            const content = document.querySelector("textarea").value;
-            console.log(content);
+        updateLengthDesc(min, max, domElement) {
+
+            const content = document.querySelector(`${domElement} textarea`).value;
             let color;
 
-            if (content.length >= 120 && content.length <= 158) {
+            if (content.length >= min && content.length <= max) {
                 color = "green";
-                console.log("true");
             } else {
                 color = "red";
             }
 
-            document.querySelector("textarea").style.borderColor = color;
-            document.querySelector("span").innerHTML = content.length;
+            document.querySelector(`${domElement} textarea`).style.borderColor = color;
+            document.querySelector(`${domElement} span`).innerHTML = content.length;
         }
     },
     // the only difference with a component instead of having the code directly in your HTML is the fact you need to create a template
     template: `
         <div class="m-3">
-            <p>Meta Description (<span></span> characters)</p>
-            <p><small>* Should remain between 120 and 158.</small></p>
-            <p><textarea v-on:keyup="updateLengthDesc"></textarea></p>
+
+            <div id="metatitle">
+                <p>Meta Title (<span></span> characters)</p>
+                <p><small>* Should remain between 50 and 60.</small></p>
+                <p><textarea rows="5" cols="30" v-on:keyup="updateLengthDesc(50, 60, '#metatitle')"></textarea></p>
+            </div>
+
+            <div id="metadescription">
+                <p>Meta Description (<span></span> characters)</p>
+                <p><small>* Should remain between 120 and 158.</small></p>
+                <p><textarea rows="5" cols="30" v-on:keyup="updateLengthDesc(120, 158, '#metadescription')"></textarea></p>
+            </div>
+
         </div>`
 });
